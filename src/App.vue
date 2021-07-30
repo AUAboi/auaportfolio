75f8c00546deba38aa3432bb391937c34a64ece0
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2021-07-30 12:11:17 +0500

    scafolding project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <main>
+    <Home />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "@/components/layouts/Header";
+import Home from "@/components/Home";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+    Header,
+		Home
+	}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,19 @@
+<template>
+	<header>
+		<h1>Ahsan_AUA</h1>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>Skills</li>
+				<li>Projects</li>
+				<li>About</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header"
+};
+</script>
