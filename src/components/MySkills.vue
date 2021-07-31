9425928e254a16a64816e427069d0e1b2b6fff22
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2021-08-01 00:33:33 +0500

    About section base done

---
+++ src/components/MySkills.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<h1 class="section-heading">My Skills</h1>
+		<article>
+			<ul>
+				<li>Frontend</li>
+				<li>Backend</li>
+			</ul>
+			<div></div>
+		</article>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "MySkills"
+};
+</script>
+
+<style>
+</style>
