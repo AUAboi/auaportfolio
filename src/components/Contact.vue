fca83bb2a44a8a8163666ceaef640dca4939edf9
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2021-08-17 20:59:04 +0500

    afas

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -45,7 +45,9 @@
 					</div>
 				</div>
 			</div>
-			<div
+			<form
+				name="contact"
+				netlify
 				class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
 			>
 				<h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
@@ -107,7 +109,7 @@
 					Chicharrones blog helvetica normcore iceland tousled brook viral
 					artisan.
 				</p>
-			</div>
+			</form>
 		</div>
 	</section>
 </template>
