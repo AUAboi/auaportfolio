bcb8d9f212f301736df6f68bd2347ff19f022df8
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2021-08-17 20:55:44 +0500

    form change for netlify

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -98,7 +98,7 @@
 					<p>{{ submit }}!</p>
 				</div>
 				<button
-					@click.prevent="sendContactFormData"
+					type="submit"
 					class="text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg"
 				>
 					Button
