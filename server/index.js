1cd1c74871be673cd7a8c768b8fa5a1a0ecce7de
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2021-08-19 12:49:43 +0500

    cors fix

--- server/index.js
+++ server/index.js
@@ -4,17 +4,16 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-const corsOptions = {
-  origin: process.env.VUE_APP_URL,
-  optionsSuccessStatus: 200,
-};
-
 dotenv.config();
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "https://dreamy-bell-301b38.netlify.app/",
+  })
+);
 //routes
 const mail = require("./routes/mail");
 
