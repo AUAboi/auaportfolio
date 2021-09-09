ac6adf54b388d3f9baecee845af58abc53d5f85f
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2021-09-09 14:45:43 +0500

    rate limit

--- server/index.js
+++ server/index.js
@@ -4,9 +4,17 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-const MongoClient = require("mongodb").MongoClient;
-
-MongoClient.connect("mongodb-connection-string", (err, client) => {});
+const rateLimit = require("express-rate-limit");
+const apiRequestLimiter = rateLimit({
+  windowMs: 1 * 60 * 60 * 1000, // 1 Hour
+  max: 5,
+  handler: function(req, res) {
+    return res.status(429).json({
+      message:
+        "Error occured: You cant do that too many times. Wait a few minutes",
+    });
+  },
+});
 
 dotenv.config();
 
@@ -20,10 +28,12 @@ app.use(express.urlencoded({ extended: true }));
 //routes
 const mail = require("./routes/mail");
 
-app.use("/api/send-mail", cors(corsOptions), mail);
+app.use("/api/send-mail", cors(corsOptions), apiRequestLimiter, mail);
+
 app.get("/", (req, res) => {
   res.send(`Welcome to ${process.env.VUE_APP_URL}`);
 });
+
 app.listen(port, () => {
   console.log(`Server is listening at ${port}`);
 });
